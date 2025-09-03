b80ed3f12d34eccc2b16926bd290022788c548dc
FranBowden <francesca.bowden22@bathspa.ac.uk>
2025-09-03 17:45:46 +0100

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -46,10 +46,11 @@ app.get("/api/direction", async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.resolve("dist")));
 
+// Catch-all route for SPA
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.resolve("dist/index.html"));
 });
 
 app.listen(PORT, () =>
