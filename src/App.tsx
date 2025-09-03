25231d70d20ef56c1f84eac8664f0158a7c8098f
FranBowden <francesca.bowden22@bathspa.ac.uk>
2025-09-03 17:36:16 +0100

    slash issues

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ const App: React.FC = () => {
     const fetchBenches = async () => {
       try {
         const res = await fetch(
-          `${API_URL}api/benches?lat=${userLocation.lat}&lng=${userLocation.lng}`
+          `${API_URL}/api/benches?lat=${userLocation.lat}&lng=${userLocation.lng}`
         );
         const benchesData: Bench[] = (await res.json()).filter(
           (b: Bench) => typeof b.lat === "number" && typeof b.lng === "number"
